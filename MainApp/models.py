1f6c4d9f8da1f8ff129a8601036cbaa80bdc6690
nikita <popov.nikita280490@gmail.com>
2023-04-20 13:06:01 +0300

    модуль 5 выполнена форма form.py

--- MainApp/models.py
+++ MainApp/models.py
@@ -1,8 +1,15 @@
 from django.db import models
 
+langs = (
+    ('cpp', 'C++'),
+    ('py', 'python'),
+    ('js', 'javascript')
+)
+
+
 
 class Snippet(models.Model):
     name = models.CharField(max_length=100)
-    lang = models.CharField(max_length=30)
+    lang = models.CharField(max_length=30, choices=langs)
     code = models.TextField(max_length=5000)
     creation_date = models.DateTimeField(auto_now=True)
--- MainApp/views.py
+++ MainApp/views.py
@@ -1,6 +1,7 @@
 from django.http import Http404
 from django.shortcuts import render, redirect
-from .models import Snippet
+from .models import Snippet    
+from MainApp.forms import SnippetForm
 
 
 def index_page(request):
@@ -9,10 +10,19 @@ def index_page(request):
 
 
 def add_snippet_page(request):
+    form = SnippetForm()
 
-    context = {'pagename': 'Добавление нового сниппета'}
-    return render(request, 'pages/add_snippet.html', context)
+    if request.method == 'GET':
 
+        context = {'pagename': 'Добавление нового сниппета',
+                "form": form}
+        return render(request, 'pages/add_snippet.html', context)
+    if request.method == 'POST':
+        form = SnippetForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('snippets-list')
+        return render(request, 'pages/add_snippet.html', {"form":form})
 
 def snippets_page(request):
     snippets = Snippet.objects.all()
@@ -31,11 +41,16 @@ def snippet_detail(request, id):
         'snippet': snippet,
     })
 
-def snippet_create(request):
-    if request.method == 'POST':
-        name = request.POST['name']
-        lang = request.POST['lang']
-        code = request.POST['code']
-        snippet = Snippet(name=name, lang=lang, code=code)
-        snippet.save()
-        return redirect('snippets-list')
+#def snippet_create(request):
+#    if request.method == 'POST':
+#        name = request.POST['name']
+#        lang = request.POST['lang']
+#        code = request.POST['code']
+#        snippet = Snippet(name=name, lang=lang, code=code)
+#        snippet.save()
+#        return redirect('snippets-list')
+
+
+def create_snippet(request):
+   form = SnippetForm()
+   return render(request, 'add_snippet.html', {'form': form})
