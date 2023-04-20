16117844ebea0277852a4b0f329a2a24105cc9ae
nikita <popov.nikita280490@gmail.com>
2023-04-20 11:30:52 +0300

    модуль 5 выполнены задания по #12 страница просмотра снипетов из бд

--- MainApp/views.py
+++ MainApp/views.py
@@ -1,5 +1,6 @@
 from django.http import Http404
 from django.shortcuts import render, redirect
+from .models import Snippet
 
 
 def index_page(request):
@@ -8,10 +9,15 @@ def index_page(request):
 
 
 def add_snippet_page(request):
+
     context = {'pagename': 'Добавление нового сниппета'}
     return render(request, 'pages/add_snippet.html', context)
 
 
 def snippets_page(request):
-    context = {'pagename': 'Просмотр сниппетов'}
+    snippets = Snippet.objects.all()
+    context = {
+        'pagename': 'Просмотр сниппетов',
+        'snippets': snippets,
+    }
     return render(request, 'pages/view_snippets.html', context)
